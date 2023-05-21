0f90e2e6ad9bdbd1fbc729a83e8ec775b3b1be43
Daniel Nolan <danielnolan85@yahoo.com>
2023-05-21 12:51:52 -0700

    mailchimp mass mail

--- controllers/chat.js
+++ controllers/chat.js
@@ -5,11 +5,15 @@ const MassMessage = require('../models/massMessages');
 const nodemailer = require('nodemailer');
 const axios = require('axios');
 const mailchimp = require('@mailchimp/mailchimp_marketing');
+const moment = require('moment');
 
 mailchimp.setConfig({
-  apiKey: 'b361b87bb09f27dc804a28f39380cbe0-us4',
+  apiKey: process.env.MAILCHIMP_KEY,
   server: 'us4',
 });
+
+// const list_id = process.env.MAILCHIMP_LIST_ID;
+// const tag_name = 'tagged';
 
 exports.accessChat = async (req, res) => {
   const { _id, u } = req.body;
@@ -169,103 +173,182 @@ exports.allMessages = async (req, res) => {
 exports.massMail = async (req, res) => {
   const { image, content, selected } = req.body.massMail;
 
-  if (!content || selected.length < 1) {
+  if (!content || selected.length < 1 || !req.body.subject) {
     return res.sendStatus(400);
   }
 
-  const sender = await User.findOne({ _id: '63dc1d2a8eb01e4110743044' }).select(
-    '_id name username email profileImage'
-  );
-  const userIds = [];
-  const chats = [];
-  const userEmails = [];
+  try {
+    const sender = await User.findOne({
+      _id: '63dc1d2a8eb01e4110743044',
+    }).select('_id name username email profileImage');
+    const chats = [];
+    const userIds = selected.map((item) => item._id);
+    const userEmails = selected.map((item) => item.email);
 
-  let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'customercare@loveisincyprus.com',
-      pass: process.env.GMAIL_AUTHORIZATION,
-    },
-    secure: true,
-  });
-
-  for (var i = 0; i < selected.length; i++) {
-    userIds.push(selected[i]._id);
-    userEmails.push(selected[i].email);
-  }
-
-  for (var i = 0; i < userIds.length; i++) {
-    const chat = await Chat.findOne({
-      users: { $size: 2, $all: [sender._id, userIds[i]] },
-    });
-    chats.push(chat);
-  }
-
-  for (var i = 0; i < chats.length; i++) {
-    var newMessage = {
-      sender,
-      content,
-      chat: chats[i],
-      image,
-    };
-    var message = await Message.create(newMessage);
-    message = await message.populate(
-      'sender',
-      'name username email profileImage'
-    );
-    message = await message.populate('chat');
-    message = await User.populate(message, {
-      path: 'chat.users',
-      select: 'name username email profileImage',
-    });
-
-    await Chat.findByIdAndUpdate(chats[i]._id, {
-      latestMessage: message,
-    });
-  }
-
-  const massMessage = await MassMessage.create({ content, image });
-
-  for (const email of userEmails) {
-    let mailOptions = {
-      from: 'customercare@loveisincyprus.com',
-      to: email,
-      subject: req.body.subject,
-      html: `
-      ${content}
-      `,
-    };
-    if (Object.keys(image).length > 0) {
-      const imageResponse = await axios.get(image.url, {
-        responseType: 'arraybuffer',
+    for (var i = 0; i < userIds.length; i++) {
+      const chat = await Chat.findOne({
+        users: { $size: 2, $all: [sender._id, userIds[i]] },
       });
-      const imageBuffer = Buffer.from(imageResponse.data, 'binary');
-      mailOptions.attachments = [
-        {
-          filename: 'image.jpg',
-          content: imageBuffer,
-        },
-      ];
+      chats.push(chat);
     }
-    transporter.sendMail(mailOptions, (err, response) => {
-      if (err) {
-        res.send(err);
-      } else {
-        res.send('Success');
-      }
-    });
-  }
-  transporter.close();
 
-  const sendNotif = await User.updateMany(
-    { _id: { $in: userIds } },
-    {
-      $push: {
-        messages: { sender: '63dc1d2a8eb01e4110743044' },
-      },
+    for (var i = 0; i < chats.length; i++) {
+      var newMessage = {
+        sender,
+        content,
+        chat: chats[i],
+        image,
+      };
+      var message = await Message.create(newMessage);
+      message = await message.populate(
+        'sender',
+        'name username email profileImage'
+      );
+      message = await message.populate('chat');
+      message = await User.populate(message, {
+        path: 'chat.users',
+        select: 'name username email profileImage',
+      });
+
+      await Chat.findByIdAndUpdate(chats[i]._id, {
+        latestMessage: message,
+      });
     }
-  );
-  res.json(message);
+
+    const massMessage = await MassMessage.create({ content, image });
+
+    const currentDate = new Date();
+    const timestamp = new Date().getTime();
+    const daysThreshold = 7;
+    const listsToDelete = [];
+
+    const getLists = async () => {
+      const response = await mailchimp.lists.getAllLists();
+      return response.lists;
+    };
+
+    const getDaysSinceDate = (date) => {
+      const timeDiff = Math.abs(currentDate.getTime() - date.getTime());
+      return Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+    };
+
+    const checkListForDeletion = async (list) => {
+      const campaignsResponse = await mailchimp.campaigns.list({
+        list_id: list.id,
+        count: 1,
+        sort_field: 'send_time',
+        sort_dir: 'DESC',
+      });
+
+      if (campaignsResponse.campaigns.length === 0) {
+        const listCreatedDate = new Date(list.date_created);
+        const daysSinceCreation = getDaysSinceDate(listCreatedDate);
+
+        if (daysSinceCreation > daysThreshold) {
+          listsToDelete.push(list);
+        }
+      } else {
+        const lastCampaignDate = new Date(
+          campaignsResponse.campaigns[0].send_time
+        );
+        const daysSinceLastCampaign = getDaysSinceDate(lastCampaignDate);
+
+        if (daysSinceLastCampaign > daysThreshold) {
+          listsToDelete.push(list);
+        }
+      }
+    };
+
+    const deleteLists = async () => {
+      const allLists = await getLists();
+
+      for (const list of allLists) {
+        await checkListForDeletion(list);
+      }
+
+      for (const listToDelete of listsToDelete) {
+        await mailchimp.lists.deleteList(listToDelete.id);
+      }
+    };
+
+    const createAndPopulateList = async () => {
+      await deleteLists();
+
+      const newListResponse = await mailchimp.lists.createList({
+        name: `Love is in Cyprus - ${timestamp}`,
+        contact: {
+          company: 'Aquilion Limited',
+          address1: 'Carewatch Barnet, Apex House',
+          city: 'Grand Arcade',
+          state: 'London',
+          zip: 'N12 0EH',
+          country: 'United Kingdom',
+        },
+        permission_reminder:
+          'You are receiving this email because you have opted in to receive occasional updates from www.loveisincyprus.com',
+        email_type_option: true,
+        campaign_defaults: {
+          from_name: 'Love Is In Cyprus',
+          from_email: 'customercare@loveisincyprus.com',
+          subject: 'Latest update from Love is in Cyprus',
+          language: 'en',
+        },
+      });
+
+      const newListId = newListResponse.id;
+
+      for (const email of userEmails) {
+        await mailchimp.lists.addListMember(newListId, {
+          email_address: email,
+          status: 'subscribed',
+        });
+      }
+
+      const campaign = await mailchimp.campaigns.create({
+        type: 'regular',
+        recipients: {
+          list_id: newListId,
+        },
+        settings: {
+          subject_line: req.body.subject,
+          from_name: 'Love Is In Cyprus',
+          reply_to: 'customercare@loveisincyprus.com',
+          from_email: 'customercare@loveisincyprus.com',
+        },
+      });
+
+      const campaignContent = await mailchimp.campaigns.setContent(
+        campaign.id,
+        {
+          html: `
+        <div style="text-align: center">
+        <img src="https://res.cloudinary.com/dg9blonbn/image/upload/v1684603773/logo_qinzhi.png" alt="logo" style="width: 100px; height: 100px; margin: 20px 0;">
+        </div>
+        ${content}${
+            Object.keys(image).length !== 0
+              ? `<img src="${image.url}" alt="Love is in Cyprus" style="max-width: 400px; height: auto;">`
+              : ''
+          }`,
+        }
+      );
+
+      const sendCampaign = await mailchimp.campaigns.send(campaign.id);
+    };
+
+    createAndPopulateList();
+
+    const sendNotif = await User.updateMany(
+      { _id: { $in: userIds } },
+      {
+        $push: {
+          messages: { sender: '63dc1d2a8eb01e4110743044' },
+        },
+      }
+    );
+    res.json(message);
+  } catch (err) {
+    console.error('Error updating audience:', err);
+  }
 };
 
 exports.chatMatches = async (req, res) => {
